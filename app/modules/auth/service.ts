5914516906e7e73112d4698635f0355d32c67702
Luiz Henrique <luizhjramos@outlook.com>
2026-04-03 15:51:40 -0300

    fix: change app domain

--- app/modules/auth/service.ts
+++ app/modules/auth/service.ts
@@ -258,7 +258,7 @@ export async function bootstrapUser(
 
 export async function forgotPassword(email: string): Promise<void> {
   const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: 'https://www.ugclocal.com.br/auth/redefinir-senha',
+    redirectTo: 'https://app.ugclocal.com.br/auth/redefinir-senha',
   });
   if (error) throw new Error(error.message);
 }
